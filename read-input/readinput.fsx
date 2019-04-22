815cff2312aac66ce9d28e24c278e4eae3bcc03f
Joseph Mckenzi <josephmckenzieaz@yahoo.com>
2019-04-22 18:41:41 -0400

    added read input from user

---
+++ read-input/readinput.fsx
@@ -0,0 +1,14 @@
+
+let choose = 
+    "Please choose a Newfie: "
+
+printf "%s" choose
+
+let userinput = System.Console.ReadLine(); 
+
+let newfies = 
+    "I love newfies , I dont have a favorite however ..... my baby is "
+
+let complete = newfies + userinput
+
+printfn "%s" complete
